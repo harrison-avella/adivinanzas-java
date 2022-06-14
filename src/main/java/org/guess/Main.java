3e737437da29f8b152789ed3fe60f4ee6bcb1f21
Jorge Yepes <p747andres@gmail.com>
2022-06-13 23:44:40 -0500

    Creacion Codigo base javafx

--- src/main/java/org/guess/Main.java
+++ src/main/java/org/guess/Main.java
@@ -1,7 +1,37 @@
 package org.guess;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    //Cargar configuraciones Iniciales
+    public void init() {
+        
+    }
+    //Cargar metodos UI
+     public void start(Stage PrimerEscenario) {
+
+        StackPane root=new StackPane();
+        Scene escenario=new Scene(root,500,500);
+
+        PrimerEscenario.setScene(escenario);
+        PrimerEscenario.setTitle("Animalia");
+        PrimerEscenario.show();
+     }
+     //Operaciones de finalizacion de la aplicacion
+     public void Stop() {
+        
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //metodo que permite lanzar la ventana inicial
+        launch(args);
     }
 }
